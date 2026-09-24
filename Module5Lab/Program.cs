2fa82748f0a40fc80433ac7d3352fb8545939eb4
Hassan03k <hassan.mohamed03k@gmail.com>
2026-09-23 21:33:18 -0500

    Game Fun

---
+++ Module5Lab/Program.cs
@@ -0,0 +1,17 @@
+﻿using Module5Lab;
+
+Player player = new Player("Frankenstein", 1);
+player.AddPoints(100);
+
+Console.WriteLine("Welcome to the game! Your score is: " + player.GetScore() + " You have " + player.GetLivesLeft() + " lives left!");
+
+player.Kill();
+
+Console.WriteLine("An orc attacks you. Sorry, you were killed. You have " + player.GetLivesLeft() + " lives left");
+
+SuperPlayer superPlayer = new SuperPlayer();
+
+superPlayer.AddPoints(200);
+superPlayer.Fly();
+
+Console.WriteLine($"SuperPlayer score: {superPlayer.GetScore()}, Lives left: {superPlayer.GetLivesLeft()}");
---
+++ Module5Lab/SuperPlayer.cs
@@ -0,0 +1,10 @@
+namespace Module5Lab
+{
+    internal class SuperPlayer : Player
+    {
+        public void Fly()
+        {
+           Console.WriteLine("SuperPlayer is flying!"); 
+        }
+    }
+}
